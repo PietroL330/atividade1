ba4a1dda3776d555d19efdd7676036382c1a9675
PietroL330 <plessa330@gmail.com>
2026-04-13 14:52:00 -0300

    atividade dirigir idade

---
+++ atividade1/atividade1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividade1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            Console.WriteLine("digite a idade");
+            idade = int.Parse(Console.ReadLine());
+
+            if (idade >= 18)
+            {
+                Console.WriteLine("pode dirigir!!");
+            }
+            else
+            {
+                Console.WriteLine("infelizmente não pode dirigir");
+            }
+
+
+
+
+
+        }
+    }
+}
